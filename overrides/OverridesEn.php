e03810fb9d058214041ee3abfd2e78d7fa0e2ca8
awjrichards <arichards@wikimedia.org>
2012-08-22 18:20:31 -0700

    Adding overrides to allow for extending imported ISO-3166-2 data

---
+++ overrides/OverridesEn.php
@@ -0,0 +1,300 @@
+<?php
+
+/**
+ * Contains any overrides or custom additions to generated data
+ *
+ * Useful in the event that data from the ISO-3166-2 data source is
+ * incorrect, or missing.
+ */
+
+// Malta (missing from commondatahub dump)
+$subdivisions['MT']['01'] = array(
+'name' => 'Attard',
+'level' => 'local council',
+);
+$subdivisions['MT']['02'] = array(
+'name' => 'Balzan',
+'level' => 'local council',
+);
+$subdivisions['MT']['03'] = array(
+'name' => 'Birgu',
+'level' => 'local council',
+);
+$subdivisions['MT']['04'] = array(
+'name' => 'Birkirkara',
+'level' => 'local council',
+);
+$subdivisions['MT']['05'] = array(
+'name' => 'Birżebbuġa',
+'level' => 'local council',
+);
+$subdivisions['MT']['06'] = array(
+'name' => 'Cospicua',
+'level' => 'local council',
+);
+$subdivisions['MT']['07'] = array(
+'name' => 'Dingli',
+'level' => 'local council',
+);
+$subdivisions['MT']['08'] = array(
+'name' => 'Fgura',
+'level' => 'local council',
+);
+$subdivisions['MT']['09'] = array(
+'name' => 'Floriana',
+'level' => 'local council',
+);
+$subdivisions['MT']['10'] = array(
+'name' => 'Fontana',
+'level' => 'local council',
+);
+$subdivisions['MT']['11'] = array(
+'name' => 'Gudja',
+'level' => 'local council',
+);
+$subdivisions['MT']['12'] = array(
+'name' => 'Gżira',
+'level' => 'local council',
+);
+$subdivisions['MT']['13'] = array(
+'name' => 'Għajnsielem',
+'level' => 'local council',
+);
+$subdivisions['MT']['14'] = array(
+'name' => 'Għarb',
+'level' => 'local council',
+);
+$subdivisions['MT']['15'] = array(
+'name' => 'Għargħur',
+'level' => 'local council',
+);
+$subdivisions['MT']['16'] = array(
+'name' => 'Għasri',
+'level' => 'local council',
+);
+$subdivisions['MT']['17'] = array(
+'name' => 'Għaxaq',
+'level' => 'local council',
+);
+$subdivisions['MT']['18'] = array(
+'name' => 'Ħamrun',
+'level' => 'local council',
+);
+$subdivisions['MT']['19'] = array(
+'name' => 'Iklin',
+'level' => 'local council',
+);
+$subdivisions['MT']['20'] = array(
+'name' => 'Senglea',
+'level' => 'local council',
+);
+$subdivisions['MT']['21'] = array(
+'name' => 'Kalkara',
+'level' => 'local council',
+);
+$subdivisions['MT']['22'] = array(
+'name' => 'Kerċem',
+'level' => 'local council',
+);
+$subdivisions['MT']['23'] = array(
+'name' => 'Kirkop',
+'level' => 'local council',
+);
+$subdivisions['MT']['24'] = array(
+'name' => 'Lija',
+'level' => 'local council',
+);
+$subdivisions['MT']['25'] = array(
+'name' => 'Luqa',
+'level' => 'local council',
+);
+$subdivisions['MT']['26'] = array(
+'name' => 'Marsa',
+'level' => 'local council',
+);
+$subdivisions['MT']['27'] = array(
+'name' => 'Marsaskala',
+'level' => 'local council',
+);
+$subdivisions['MT']['28'] = array(
+'name' => 'Marsaxlokk',
+'level' => 'local council',
+);
+$subdivisions['MT']['29'] = array(
+'name' => 'Mdina',
+'level' => 'local council',
+);
+$subdivisions['MT']['30'] = array(
+'name' => 'Mellieħa',
+'level' => 'local council',
+);
+$subdivisions['MT']['31'] = array(
+'name' => 'Mġarr',
+'level' => 'local council',
+);
+$subdivisions['MT']['32'] = array(
+'name' => 'Mosta',
+'level' => 'local council',
+);
+$subdivisions['MT']['33'] = array(
+'name' => 'Mqabba',
+'level' => 'local council',
+);
+$subdivisions['MT']['34'] = array(
+'name' => 'Msida',
+'level' => 'local council',
+);
+$subdivisions['MT']['35'] = array(
+'name' => 'Imtarfa',
+'level' => 'local council',
+);
+$subdivisions['MT']['36'] = array(
+'name' => 'Munxar',
+'level' => 'local council',
+);
+$subdivisions['MT']['37'] = array(
+'name' => 'Nadur',
+'level' => 'local council',
+);
+$subdivisions['MT']['38'] = array(
+'name' => 'Naxxar',
+'level' => 'local council',
+);
+$subdivisions['MT']['39'] = array(
+'name' => 'Paola',
+'level' => 'local council',
+);
+$subdivisions['MT']['40'] = array(
+'name' => 'Pembroke',
+'level' => 'local council',
+);
+$subdivisions['MT']['41'] = array(
+'name' => 'Pietà',
+'level' => 'local council',
+);
+$subdivisions['MT']['42'] = array(
+'name' => 'Qala',
+'level' => 'local council',
+);
+$subdivisions['MT']['43'] = array(
+'name' => 'Qormi',
+'level' => 'local council',
+);
+$subdivisions['MT']['44'] = array(
+'name' => 'Qrendi',
+'level' => 'local council',
+);
+$subdivisions['MT']['45'] = array(
+'name' => 'Rabat Gozo',
+'level' => 'local council',
+);
+$subdivisions['MT']['46'] = array(
+'name' => 'Rabat Malta',
+'level' => 'local council',
+);
+$subdivisions['MT']['47'] = array(
+'name' => 'Safi',
+'level' => 'local council',
+);
+$subdivisions['MT']['48'] = array(
+'name' => 'St. Julian\'s',
+'level' => 'local council',
+);
+$subdivisions['MT']['49'] = array(
+'name' => 'San Ġwann',
+'level' => 'local council',
+);
+$subdivisions['MT']['50'] = array(
+'name' => 'San Lawrenz',
+'level' => 'local council',
+);
+$subdivisions['MT']['51'] = array(
+'name' => 'St. Paul\s Bay',
+'level' => 'local council',
+);
+$subdivisions['MT']['52'] = array(
+'name' => 'Sannat',
+'level' => 'local council',
+);
+$subdivisions['MT']['53'] = array(
+'name' => 'Santa Luċija',
+'level' => 'local council',
+);
+$subdivisions['MT']['54'] = array(
+'name' => 'Santa Venera',
+'level' => 'local council',
+);
+$subdivisions['MT']['55'] = array(
+'name' => 'Siġġiewi',
+'level' => 'local council',
+);
+$subdivisions['MT']['56'] = array(
+'name' => 'Sliema',
+'level' => 'local council',
+);
+$subdivisions['MT']['57'] = array(
+'name' => 'Swieqi',
+'level' => 'local council',
+);
+$subdivisions['MT']['58'] = array(
+'name' => 'Ta\' Xbiex',
+'level' => 'local council',
+);
+$subdivisions['MT']['59'] = array(
+'name' => 'Tarxien',
+'level' => 'local council',
+);
+$subdivisions['MT']['60'] = array(
+'name' => 'Valletta',
+'level' => 'local council',
+);
+$subdivisions['MT']['61'] = array(
+'name' => 'Xagħra',
+'level' => 'local council',
+);
+$subdivisions['MT']['62'] = array(
+'name' => 'Xewkija',
+'level' => 'local council',
+);
+$subdivisions['MT']['63'] = array(
+'name' => 'Xgħajra',
+'level' => 'local council',
+);
+$subdivisions['MT']['64'] = array(
+'name' => 'Żabbar',
+'level' => 'local council',
+);
+$subdivisions['MT']['65'] = array(
+'name' => 'Żebbuġ Għawdex',
+'level' => 'local council',
+);
+$subdivisions['MT']['66'] = array(
+'name' => 'Żebbuġ Malta',
+'level' => 'local council',
+);
+$subdivisions['MT']['67'] = array(
+'name' => 'Żejtun',
+'level' => 'local council',
+);
+$subdivisions['MT']['68'] = array(
+'name' => 'Żurrieq',
+'level' => 'local council',
+);
+
+// Great Brittain (commondatahub data missing countries)
+$subdivisions['GB']['SCT'] = array(
+'name' => 'Scotland',
+'level' => 'country',
+);
+$subdivisions['GB']['ENG'] = array(
+'name' => 'England',
+'level' => 'country',
+);
+$subdivisions['GB']['NIR'] = array(
+'name' => 'Northern Ireland',
+'level' => 'country',
+);
+$subdivisions['GB']['WLS'] = array(
+'name' => 'Wales',
+'level' => 'country',
+);
